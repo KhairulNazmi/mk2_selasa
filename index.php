31d446b81eefdb7f1ceca74eced4802c21f77767
KhairulNazmi <m.khairulnazmi@gmail.com>
2023-09-05 16:19:37 +0800

    siap index
    siap index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cerita</title>
+</head>
+<body>
+    <h1>Cerita Anda</h1>
+    <form method="post" action="papar.php">
+        <label>Cerita tentang diri anda:</label>
+        <textarea type="textarea" name="input" required></textarea>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
